e35926e81ad63b2e185900286b712ed98ca3aa64
hcyang1386 <hcyang1386@gmail.com>
2025-07-29 12:17:16 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,45 +1,59 @@
 import streamlit as st
 import streamlit.components.v1 as htmlviewer
+import base64
+
+# ✅ Streamlit 설정
 st.set_page_config(layout='wide', page_title='5계 분류하기 모델')
-# Title Msg#1
 st.title('This is yang Webapp!!')
 
 
-with open('./indexmy1.html', 'r', encoding='utf-8') as f:
+# ✅ 이미지 파일을 base64 문자열로 변환하는 함수
+def img_to_base64(img_path):
+    with open(img_path, 'rb') as img_file:
+        encoded = base64.b64encode(img_file.read()).decode()
+    return f'data:image/png;base64,{encoded}'
+
+
+# ✅ 이미지 base64 인코딩
+flowchart_base64 = img_to_base64('flowchart.png')   # indexmy1.html 에서 사용
+biocard_base64 = img_to_base64('biocard.png')       # indexmy2.html 에서 사용
+
+
+# ✅ HTML 파일 읽기
+with open('indexmy1.html', 'r', encoding='utf-8') as f:
     html1 = f.read()
-    f.close()    
 
-with open('./indexmy2.html', 'r', encoding='utf-8') as f:
+with open('indexmy2.html', 'r', encoding='utf-8') as f:
     html2 = f.read()
-    f.close() 
 
-#html='''
-#<html>
-#    <head>
-#        <title> this is my html </title>
-#    </head>
-#    <body>
-#        <h1>Topic</h1>
-#        <h2>SubTopic</h2>
-#    </body>
-#</html>
-#'''
 
-# Box#1(4), Box#2(1)
-col1, col2 = st.columns((4,1))
+# ✅ 이미지 경로를 base64로 치환
+html1 = html1.replace('src="flowchart.png"', f'src="{flowchart_base64}"')
+html2 = html2.replace('src="biocard.png"', f'src="{biocard_base64}"')
+
+
+# ✅ Streamlit 레이아웃 구성
+col1, col2 = st.columns((4, 1))
+
 with col1:
     with st.expander('Content #1...'):
         url = 'https://www.youtube.com/watch?v=XyEOEBsa8I4'
         st.info('Content..')
         st.video(url)
+
     with st.expander('Content #2...'):
-        #st.write(html, unsafe_allow_html=True)
-        htmlviewer.html(html1, height=2000)
+        htmlviewer.html(html1, height=2400)
+
     with st.expander('Content #3...'):
-        #st.write(html, unsafe_allow_html=True)
-        htmlviewer.html(html2, height=2000)
+        htmlviewer.html(html2, height=2400)
+
 with col2:
     with st.expander('Tips..'):
         st.info('Tips..')
+
+# ✅ 하단 저작권 표시
 st.markdown('<hr>', unsafe_allow_html=True)
-st.write('<font color="BLUE">(c)copyright. all rights reserved by skykang', unsafe_allow_html=True)
+st.write(
+    '<font color="BLUE">(c)copyright. all rights reserved by skykang</font>',
+    unsafe_allow_html=True
+)
